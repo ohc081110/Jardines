58204501541cb58957f6378f736112ac18dcd402
Omar <70787202+ohc081110@users.noreply.github.com>
2021-04-30 20:11:17 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,19 @@
+
+
+package Vistas;
+
+//import testbd.Conectate;
+
+
+public class Principal 
+{
+
+    public static void main(String[] args) 
+    {
+        NewUser nu = new NewUser (); 
+        nu.setVisible(true);
+//Conectate con = new Conectate();
+       
+    }
+    
+}
